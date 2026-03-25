c25899648f18e08518db54116175a4f9da05955d
marufislam8924 <marufislam8924@gmail.com>
2026-03-25 19:22:41 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,12 @@ import "./globals.css";
 
 export const metadata = {
   title: "Skilldojo",
+  description: "Learn Japanese",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:wght@300;400;500&family=Noto+Serif+JP:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
