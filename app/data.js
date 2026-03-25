4470feb151b744e3e243a5c15c869151674a691c
marufislam8924 <marufislam8924@gmail.com>
2026-03-26 01:20:23 +0600

    Add files via upload

---
+++ app/data.js
@@ -0,0 +1,86 @@
+export const hiraganaLessons = [
+  {
+    id: 1, kana: "あ", name: "Row A",
+    chars: [{ k: "あ", r: "a" }, { k: "い", r: "i" }, { k: "う", r: "u" }, { k: "え", r: "e" }, { k: "お", r: "o" }],
+  },
+  {
+    id: 2, kana: "か", name: "Row KA",
+    chars: [{ k: "か", r: "ka" }, { k: "き", r: "ki" }, { k: "く", r: "ku" }, { k: "け", r: "ke" }, { k: "こ", r: "ko" }],
+  },
+  {
+    id: 3, kana: "さ", name: "Row SA",
+    chars: [{ k: "さ", r: "sa" }, { k: "し", r: "shi" }, { k: "す", r: "su" }, { k: "せ", r: "se" }, { k: "そ", r: "so" }],
+  },
+  {
+    id: 4, kana: "た", name: "Row TA",
+    chars: [{ k: "た", r: "ta" }, { k: "ち", r: "chi" }, { k: "つ", r: "tsu" }, { k: "て", r: "te" }, { k: "と", r: "to" }],
+  },
+  {
+    id: 5, kana: "な", name: "Row NA",
+    chars: [{ k: "な", r: "na" }, { k: "に", r: "ni" }, { k: "ぬ", r: "nu" }, { k: "ね", r: "ne" }, { k: "の", r: "no" }],
+  },
+  {
+    id: 6, kana: "は", name: "Row HA",
+    chars: [{ k: "は", r: "ha" }, { k: "ひ", r: "hi" }, { k: "ふ", r: "fu" }, { k: "へ", r: "he" }, { k: "ほ", r: "ho" }],
+  },
+  {
+    id: 7, kana: "ま", name: "Row MA",
+    chars: [{ k: "ま", r: "ma" }, { k: "み", r: "mi" }, { k: "む", r: "mu" }, { k: "め", r: "me" }, { k: "も", r: "mo" }],
+  },
+  {
+    id: 8, kana: "や", name: "Row YA",
+    chars: [{ k: "や", r: "ya" }, { k: "ゆ", r: "yu" }, { k: "よ", r: "yo" }],
+  },
+  {
+    id: 9, kana: "ら", name: "Row RA",
+    chars: [{ k: "ら", r: "ra" }, { k: "り", r: "ri" }, { k: "る", r: "ru" }, { k: "れ", r: "re" }, { k: "ろ", r: "ro" }],
+  },
+  {
+    id: 10, kana: "わ", name: "Row WA",
+    chars: [{ k: "わ", r: "wa" }, { k: "を", r: "wo" }, { k: "ん", r: "n" }],
+  },
+  {
+    id: 11, kana: "が", name: "Row GA",
+    chars: [{ k: "が", r: "ga" }, { k: "ぎ", r: "gi" }, { k: "ぐ", r: "gu" }, { k: "げ", r: "ge" }, { k: "ご", r: "go" }],
+  },
+  {
+    id: 12, kana: "ざ", name: "Row ZA",
+    chars: [{ k: "ざ", r: "za" }, { k: "じ", r: "ji" }, { k: "ず", r: "zu" }, { k: "ぜ", r: "ze" }, { k: "ぞ", r: "zo" }],
+  },
+  {
+    id: 13, kana: "だ", name: "Row DA",
+    chars: [{ k: "だ", r: "da" }, { k: "ぢ", r: "di" }, { k: "づ", r: "du" }, { k: "で", r: "de" }, { k: "ど", r: "do" }],
+  },
+  {
+    id: 14, kana: "ば", name: "Row BA",
+    chars: [{ k: "ば", r: "ba" }, { k: "び", r: "bi" }, { k: "ぶ", r: "bu" }, { k: "べ", r: "be" }, { k: "ぼ", r: "bo" }],
+  },
+  {
+    id: 15, kana: "ぱ", name: "Row PA",
+    chars: [{ k: "ぱ", r: "pa" }, { k: "ぴ", r: "pi" }, { k: "ぷ", r: "pu" }, { k: "ぺ", r: "pe" }, { k: "ぽ", r: "po" }],
+  },
+  {
+    id: 16, kana: "きゃ", name: "Compound KYA",
+    chars: [{ k: "きゃ", r: "kya" }, { k: "きゅ", r: "kyu" }, { k: "きょ", r: "kyo" }],
+  },
+  {
+    id: 17, kana: "しゃ", name: "Compound SHA",
+    chars: [{ k: "しゃ", r: "sha" }, { k: "しゅ", r: "shu" }, { k: "しょ", r: "sho" }],
+  },
+  {
+    id: 18, kana: "ちゃ", name: "Compound CHA",
+    chars: [{ k: "ちゃ", r: "cha" }, { k: "ちゅ", r: "chu" }, { k: "ちょ", r: "cho" }],
+  },
+  {
+    id: 19, kana: "にゃ", name: "Compound NYA",
+    chars: [{ k: "にゃ", r: "nya" }, { k: "にゅ", r: "nyu" }, { k: "にょ", r: "nyo" }],
+  },
+  {
+    id: 20, kana: "ひゃ", name: "Compound HYA",
+    chars: [{ k: "ひゃ", r: "hya" }, { k: "ひゅ", r: "hyu" }, { k: "ひょ", r: "hyo" }],
+  },
+  {
+    id: 21, kana: "みゃ", name: "Compound MYA",
+    chars: [{ k: "みゃ", r: "mya" }, { k: "みゅ", r: "myu" }, { k: "みょ", r: "myo" }],
+  },
+];
