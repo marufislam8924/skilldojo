4916b143c14cd9aee8f08db48b520201f16d4566
marufislam8924 <marufislam8924@gmail.com>
2026-03-26 01:19:46 +0600

    Delete app directory

--- app/flashcards/page.js
+++
@@ -1,7 +0,0 @@
-export default function Flashcards() {
-  return (
-    <main className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl">Flashcards Coming Soon 🚀</h1>
-    </main>
-  );
-}
