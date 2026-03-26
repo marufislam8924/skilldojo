1012975c8ba3952c71a660286a3be6010a1e4b6b
marufislam8924 <marufislam8924@gmail.com>
2026-03-26 11:55:12 +0600

    Update page.js

--- app/katakana/[lesson]/page.js
+++ app/katakana/[lesson]/page.js
@@ -1,19 +1,11 @@
 "use client";
 import { useParams } from "next/navigation";
-import { katakanaLessons } from "../../../data";
-import LessonView from "../../../components/LessonView";
+import { katakanaLessons } from "../../data";
+import LessonView from "../../components/LessonView";
 
 export default function KatakanaLessonPage() {
   const { lesson } = useParams();
   const lessonId = parseInt(lesson);
   const data = katakanaLessons.find((l) => l.id === lessonId) || katakanaLessons[0];
-
-  return (
-    <LessonView
-      lessonId={lessonId}
-      data={data}
-      courseSlug="katakana"
-      totalLessons={21}
-    />
-  );
+  return <LessonView lessonId={lessonId} data={data} courseSlug="katakana" totalLessons={21}/>;
 }
