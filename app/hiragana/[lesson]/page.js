9234d8724227c8a90b44af0f1be326aa9d77dc32
marufislam8924 <marufislam8924@gmail.com>
2026-03-26 12:21:57 +0600

    Update page.js

--- app/hiragana/[lesson]/page.js
+++ app/hiragana/[lesson]/page.js
@@ -1,11 +1,11 @@
 "use client";
 import { useParams } from "next/navigation";
-import { hiraganaLessons } from "../../data";
-import LessonView from "../../components/LessonView";
+import { hiraganaLessons } from "../../../data";
+import LessonView from "../../../components/LessonView";
 
 export default function HiraganaLessonPage() {
   const { lesson } = useParams();
   const lessonId = parseInt(lesson);
   const data = hiraganaLessons.find((l) => l.id === lessonId) || hiraganaLessons[0];
-  return <LessonView lessonId={lessonId} data={data} courseSlug="hiragana" totalLessons={21}/>;
+  return <LessonView lessonId={lessonId} data={data} courseSlug="hiragana" totalLessons={21} />;
 }
